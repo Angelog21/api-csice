b40a56c89df5322b3bc99b698a12a6000f6f3fbd
Angelog21 <angelog211198@gmail.com>
2022-08-10 17:20:27 -0400

    ajustes

--- app/Http/Controllers/ServiceRequestController.php
+++ app/Http/Controllers/ServiceRequestController.php
@@ -89,6 +89,7 @@ class ServiceRequestController extends Controller
             return response([
                 "success"=>false,
                 "message" => "Ha ocurrido un error en el servidor.",
+                "error" => $e
             ],500);
         }
     }
