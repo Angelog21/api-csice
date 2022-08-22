e8b276603fc98948fbb09b9289c4e5834e18880b
Angelog21 <angelog211198@gmail.com>
2022-08-22 17:51:58 -0400

    ajuste correo de aprobación

--- resources/views/emails/response_request.blade.php
+++ resources/views/emails/response_request.blade.php
@@ -6,7 +6,20 @@
     Hola {{ $requestService->user->name }}, gracias por solicitar el servicio <h2><strong>{{$requestService->service->name}}</strong></h2>
     a través de nuestra plataforma.
     @if ($requestService->status == 'Aprobado')
-        <p>Es de nuestro agrado informarle que su soclicitud ha sido aprobada, debe descargar el archivo adjunto al correo para proceder la entrega de los recaudos y así poder ejecutar la entrega del servicio</p>
+        <p>
+            Es de nuestro agrado informarle que para culminar el proceso de su solicitud es necesario que adjunte a través de nuestra plataforma de Sistema de Gestión de Servicios de Firma electrónica los archivos correspondientes, es decir, en caso de ser: 
+
+            <ul>
+                <li>
+                    Empleado Público adjuntar el RIF de la Institución, nombramiento o Gaceta y CI. del Signatario.
+                </li>
+                <li>
+                </li>
+                <li>
+                    Persona Jurídica adjuntar RIF de la empresa, CI del signatario y Acta Constitutiva y nombramiento 
+                </li>
+            </ul>
+        </p>
     @else
         <p>
             Para esta ocasión su solicitud ha sido rechazada
