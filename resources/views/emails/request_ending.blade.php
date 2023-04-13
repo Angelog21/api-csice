46475dee8001c325a411a88c69b3dc0123de4d0b
alfredotierraaudio <alfredo@tierra.audio>
2023-04-13 11:21:50 -0400

    changes

--- resources/views/emails/request_ending.blade.php
+++ resources/views/emails/request_ending.blade.php
@@ -3,7 +3,10 @@
     <meta charset="utf-8">
 </head>
 <body>
-    <h2>Hola {{ $name }}, gracias por preferirnos al usar nuestra plataforma para tu solicitud de servicios!</h2>
+    <h2>
+        Hola  @if(isset($name)) {{ $name }}, @endif
+        gracias por preferirnos al usar nuestra plataforma para tu solicitud de servicios!
+    </h2>
     <p>Te informamos que tu solicitud se ha finalizado de manera satisfactoria.</p>
     <p>Si deseas solicitar algún otro servicio, puedes realizarlo a través de nuestra plataforma, estaremos a gusto de atenderte.</p>
 </body>
