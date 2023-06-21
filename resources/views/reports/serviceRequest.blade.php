ac22c8b4d5b8a609906a60bc46209e22ed08bcb6
alexjrf03 <alexjrf27@gmail.com>
2023-06-21 15:25:01 -0400

    creacion de ost bcv

--- resources/views/reports/serviceRequest.blade.php
+++ resources/views/reports/serviceRequest.blade.php
@@ -23,10 +23,14 @@
           text-align: center;
         }
 
+        .tableSpecial{
+          background: white;
+        }
+
     </style>
 </head>
 <body>
-    <img src="{{public_path().'/img/HeaderPDF.png'}}" alt="">
+  <img src="{{public_path().'/img/HeaderPDF.png'}}" alt="">
     <div style="margin-top:20px;width:703px">
       <table style="margin-top:10px" border="1">
           <tr>
@@ -213,6 +217,7 @@
       </div>
     @endif
 
+    @if($requestService->user->id !== 4)
     <div style="margin-top:80px;width:703px; text-align:center;">
       <h3>Condiciones de la oferta</h3>
       <table style="margin-top:10px" border="1">
@@ -344,5 +349,251 @@
         </tr>
       </table>
     </div>
+
+    @else
+    <div style="margin-top:80px;width:703px; text-align:center;">
+      <h3>Condiciones de la oferta</h3>
+      <table style="margin-top:10px" border="1">
+          <tr>
+            <th>1.- CONDICIONES GENERALES DE LA OFERTA</th>
+          </tr>
+          <tr>
+            <th>1.1.- ACEPTACIÓN DE LA OFERTA:</th>
+          </tr>
+          <tr>
+            <td>
+              <b>
+                En caso de aceptación de la presente oferta, el cliente deberá enviar una copia de ésta firmada y sellada, junto al comprobante bancario del pago o en caso de no poder realizar el pago por vencimiento de la firma enviar un documento de compromiso tipo Orden de Compra u Orden de Servicio, al mismo correo electrónico (atencion.cliente.psc.fii@gmail.com) donde recibió esta Oferta de Servicio.
+                  <br><br>
+                  <span>DATOS QUE DEBE CONTENER LA ORDEN DE SERVICIO U ORDEN DE COMPRA:</span>
+                  <br><br>
+
+                  <ul style="text-align:start">
+                    <li><b>Datos completos de Facturación:</b> Nombre de la Empresa, RIF, Dirección Fiscal Completa, Persona Contacto, Número de Teléfono.
+                    </li>
+                    <li><b>Datos del Cliente para el Informe:</b> Nombre de la Empresa, RIF, Dirección Fiscal Completa, Persona Contacto, Número de Teléfono.</li>
+                  </ul>
+              </b>
+            </td>
+          </tr>
+      </table>
+      <table style="margin-top:-3px" border="1">
+        <tr>
+          <th>1.2 - FORMAS DE PAGO</th>
+        </tr>
+        <tr>
+          <td style="text-align:start">
+            <b>
+              Para todo lo relacionado con los pagos y antes de realizar el mismo, se recomienda al cliente comunicarse con la persona contacto de Administración para aclarar cualquier detalle relacionado con la Oferta.
+              <br><br>
+              PAGOS MEDIANTE DEPÓSITOS O TRANSFERENCIAS BANCARIA: Deberán realizarse a la <span style="color:#8f0923">Cuenta Corriente N°  0102-0552-21-0000059831, perteneciente al Banco de Venezuela, a nombre de la FUNDACIÓN INSTITUTO DE INGENIERÍA</span>.
+              <br><br>
+              El comprobante de pago Bancario deberá hacerse llegar a la Oficina de Facturación y Cobranza de la Fundación Instituto de Ingeniería, a la Persona Contacto de Administración; bien sea personalmente o por correo electronico.
+            </b>
+            <br><br>
+          </td>
+        </tr>
+      </table>
+  
+      <table style="margin-top:-3px" border="1">
+        <tr>
+          <th>2 - CONDICIONES PARA EL PRÉSTAMO DEL SERVICIO</th>
+        </tr>
+        <tr>
+          <td style="text-align:start">
+            <b>
+              1.- Esta oferta tendrá una vigencia de quince (15) días para su ejecución a partir de la aceptación de la misma, si no es ejecutada en el tiempo indicado el cliente deberá cancelar la diferencia de su costo inicial, en caso de que exista una variación de precios decretada por las autoridades del Instituto de Ingeniería al momento de su ejecución.
+              <br><br>
+              2.- Las condiciones y políticas del ciclo de vida del certificado electrónico, emitido por el Proveedor de Servicios de Certificación Electrónica de la Fundación Instituto de Ingeniería para la Investigación y Desarrollo Tecnológico, esta disponible en el sitio web: https://publicador-psc.fii.gob.ve/dpc
+              <br><br>
+            </b>
+          </td>
+        </tr>
+      </table>
+      <table style="margin-top:-3px" border="1">
+        <tr>
+          <th>2.1.- CONDICIONES DE PAGO:</th>
+        </tr>
+        <tr>
+          <td style="text-align:start">
+            <b>
+              Cancelar al recibir la Oferta de Servicio en el plazo de la vigencia de la misma. De no cancelar durante ese lapso volver a solicitar una Oferta de Servicio.
+            </b>
+          </td>
+        </tr>
+      </table>
+      <table style="margin-top:-3px" border="1">
+        <tr>
+          <th>2.2.- OBJETIVO GENERAL:</th>
+        </tr>
+        <tr>
+          <td style="text-align:start">
+            <b>Prestación del servicio de soporte en el componente de Firma Electrónica, así como lo referente a la generación y revocación de certificados electrónicos para ambientes de certificación y ambiente de producción, esto con el fin de contar con los mecanismos para alertar sobre las desviaciones detectadas en la operación de este servicio, de acuerdo a lo especificado en los procesos y procedimientos definidos para el mismo.</b>
+          </td>
+        </tr>
+      </table>
+      <table style="margin-top:-3px" border="1">
+        <tr>
+          <th>2.3.- NORMAS:</th>
+        </tr>
+        <tr>
+          <td style="text-align:left">
+            <b>
+              Generales:
+              <br><br>
+              Las normas a seguir para el uso de los niveles escalatorios en la prestación del servicio son las siguientes:
+              <br><br>
+              1.- La FII garantizará el cumplimiento de las normas y procedimientos señalados en la oferta.
+              <br><br>
+              2.- Será responsabilidad de la FII asegurar la divulgación, conocimiento, aplicación de los pasos y procedimientos establecidos en esta oferta, a todo el personal que recibe asistencia así como los que prestan el servicio.
+              <br><br>
+              3.- Será responsabilidad de la FII revisar este documento y actualizarlo en el momento que sea requerido por nuevas exigencias del cliente.
+              <br><br>
+              4.- Será responsabilidad de la FII notificar a los involucrados acerca de cualquier modificación de la que sea objeto la presente oferta.
+              <br><br>
+              5.- En caso de ser aprobada la presente oferta de servicios tecnológicos, será responsabilidad de la FII el diseño del documento de definición de acuerdos de nivel de servicio, contentivo de los lineamientos por los cuales se rige esta oferta, así como los términos generales para la prestación de todos los servicios y el establecimiento de las responsabilidades que tienen tanto la “FII” como “El Cliente” para el buen cumplimiento del contrato, siendo el mencionado documento objeto de firma por ambas partes involucradas cuando éstas lo determinen.
+              <br><br>
+              Específicas:
+              <br><br>
+              1.- Es responsabilidad de la FII cumplir con el esquema de niveles escalatorios descritos en la tabla expuesta en la Sección 2.4, punto 5. Estos niveles serán utilizados cada vez que se requiera generar alertas en aquellas transacciones que demanden el empleo de los servicios ofrecidos por la aplicación de firma electrónica (SOFE), las cuales hayan excedido los tiempos estimados de respuesta.
+              <br><br>
+              2.- Las áreas en las que se requeriría activar el mecanismo de escalamiento son las siguientes:
+              <br>
+              <b style="margin-left: 10px">* Administración de Glassfish.</b>
+              <br>
+              <b style="margin-left: 10px">* Clustering asociado al SOFE.</b>
+              <br>
+              <b style="margin-left: 10px">* Administración de Usuario para uso del Web Service de Firma Electrónica.</b>
+              <br>
+              <b style="margin-left: 10px">* Configuración de dominios para uso del Web Service de Firma Electrónica.</b>
+              <br>
+              <b style="margin-left: 10px">* Servicios prestados a través de la aplicación SOFE (generación y validación de la firma electrónica).</b>
+              <br>
+              <b style="margin-left: 10px">* Comunicación entre el SOFE y los distintos SIP.</b>
+              <br>
+              <b style="margin-left: 10px">* Generación y revocación de certificados electrónicos.</b>
+              <br><br>
+              3.- La comunicación del incidente podrá realizarse mediante correo electrónico o llamada telefónica.
+              <br><br>
+              4.- Una vez resuelta la situación, el responsable a cargo del nivel respectivo deberá notificar al usuario y a su supervisor de área, la resolución del incidente a través de cualquiera de las modalidades señaladas en el punto anterior.
+            </b>
+            <br><br>
+          </td>
+        </tr>
+      </table>
+      <table style="margin-top:-3px" border="1">
+        <tr>
+          <th>2.4.- ESQUEMA:</th>
+        </tr>
+        <tr>
+          <td style="text-align:left">
+            <b>
+              Para realizar la ejecución de las Jornadas que ofrece el Centro de Seguridad Informática y Certificación Electrónica (CSICE), se cumple con las siguientes especificaciones:
+              <br><br>
+              1.- El cliente detecta la irregularidad en alguna de las áreas descritas en el punto 2 de las normas específicas.
+              <br><br>
+              2.- El cliente ejecuta una llamada telefónica al primer nivel de escalamiento y espera el tiempo indicado.
+              <br><br>
+              3.- El representante del nivel contactado deberá gestionar la solución a la alarma reportada y dar respuesta de las acciones tomadas en el tiempo establecido para ello.
+              <br><br>
+              4.- De no recibir respuesta, el cliente podrá activar los niveles sucesivos disponibles.
+              <br><br>
+              5.- A continuación se muestra el nivel de escalamiento:
+              <br><br>
+            </b>
+          </td>
+        </tr>
+      </table>
+      <table border="1">
+        <tr>
+          <th class="tableSpecial">
+            NIVEL 1: PERSONAL DE GUARDIA 
+            <br> 
+            Tiempo: 30min.
+            <br> 
+            Tlfn: (0212) 5358998.
+          </th>
+
+          <th class="tableSpecial">
+            NIVEL 2: JEFE DEL CSICE 
+            <br> 
+            Tiempo: 45min.
+            <br> 
+            Tlfn: (0414) 2263729.
+          </th>
+
+          <th class="tableSpecial">
+            NIVEL 3: PRESIDENCIA 
+            <br> 
+            Tiempo: 60min,.
+            <br> 
+            Tlfn: (0416) 6223177.
+          </th>
+        </tr>
+      </table>
+      <table border="1">
+        <tr>
+          <td style="margin-top: 3px; text-align: left">
+            <b>El nivel de escalamiento para la atención del requerimiento solicitado debe estar sujeto a los tiempos establecidos en el punto anterior, de lo contrario, la solicitud no tendrá validez y debe realizarse un nuevo requerimiento.</b>
+            <br><br>
+          </td>
+        </tr>
+      </table>
+
+      <table  border="1">
+        <tr>
+          <th>DURACIÓN:</th>
+        </tr>
+        <tr>
+          <td style="text-align: left">
+            <b>El Servicio tiene una duración de un (01) año, a partir de la Fecha de aprobación de la oferta de servicio.</b>
+          </td>
+        </tr>
+      </table>
+
+      <table border="1">
+        <tr>
+          <th>FECHA DE INICIO Y CULMINACIÓN DEL SERVICIO:</th>
+        </tr>
+        <tr>
+          <td style="text-align: left">
+            <b>Fecha de Inicio: {{$requestService->start_date}}</b>
+            <br>
+            <b>Fecha de Culminación: {{$requestService->expiration_date}}</b>
+            <br><br>
+          </td>
+        </tr>
+      </table>
+
+      <table border="1">
+        <tr>
+          <th>CONDICIONES DEL SERVICIO DE SOPORTE:</th>
+        </tr>
+        <tr>
+          <td style="text-align: left">
+            <b>
+              1.- La FII se compromete a prestar soporte técnico a las operaciones directamente relacionadas con la aplicación SOFE.
+              <br><br>
+              2.- El cliente se compromete a notificar con al menos diez días hábiles de antelación cualquier cambio en los sistemas involucrados con el funcionamiento del SOFE que pueda afectar operaciones críticas.
+              <br><br>
+              3.- La FII podrá dar soporte en sitio ante fallas inesperadas de SOFE que afecten de forma directa operaciones críticas de los sistemas del cliente.
+              <br><br>
+              4.- La FII no dará, dentro del alcance de esta oferta, soporte en sitio a usuarios finales de certificados electrónicos o usuarios finales de los sistemas relacionados con SOFE. 
+              <br><br>
+              5.- El Servicio no incluye fines de semana, ni días feriados.
+              <br><br>
+              6.- No incluye los costos de emisión de certificados electrónicos, estos se cotizarán aparte en el momento de la solicitud.
+              <br><br>
+              7.- El alcance de los servicios será como máximo de 120 horas laborables en sitio, distribuidos en el lapso de duración del contrato.
+              <br><br>
+            </b>
+          </td>
+        </tr>
+      </table>
+    </div>
+    @endif
+
+
+
 </body>
 </html>
